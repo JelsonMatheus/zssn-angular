bcbc4c64da8f64d1a9324b1ebd0e71e8067ebb39
Jelson Matheus <jelsonmatheus200f@gmail.com>
2022-08-25 20:58:48 -0300

    Adiciona sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit, 
+  Component, 
+  ElementRef, 
+  OnInit, 
+  Renderer2, 
+  ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit{
 
-  constructor() { }
+  sideBar!: HTMLElement|null;
+  @ViewChild('btnToggle', {read:ElementRef}) btnToggle!: ElementRef;
 
-  ngOnInit(): void {
+  isOpen = false;
+
+  constructor(private render: Renderer2) {}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    const toggleTarget = this.btnToggle.nativeElement.dataset.target;
+    this.sideBar = document.getElementById(toggleTarget);
+  }
+
+  toggle(): void {
+    console.log('aaa');
+    const action = !this.isOpen ? 'addClass' : 'removeClass';
+    this.render[action](this.sideBar, 'close');
+    this.isOpen = !this.isOpen;
   }
 
 }
